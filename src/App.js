e0c71fb2ff364f90bf9a1548cbd641d9f68235e0
Muhamad Eman Sulaeman <37392560+muhamademan@users.noreply.github.com>
2021-12-31 16:12:09 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,85 @@
-import React from "react";
-import './App.css'
+import React, { useState } from "react";
+import './style.css'
 
-class Latihan extends React.Component {
-  constructor(props) {
-    super(props)
-    this.setState = {
-      count: 0,
-      text: ''
+const Latihan = () => {
+  const [namaPeserta, setNamaPeserta] = useState(
+    ['Alan Subagja', 'Alan Sugandi', 'Alan Sundayat']
+  )
+  const [inputNama, setInputNama] = useState('')
+  const [currentIndex, setCurrentIndex] = useState(-1)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    let newData = namaPeserta
+
+    if (currentIndex === -1) {
+      newData = [...namaPeserta, inputNama]
+    } else {
+      newData[currentIndex] = inputNama
     }
+
+    setNamaPeserta(newData)
+    setInputNama('')
   }
 
-  handleButton = () => {
-    this.setState(prev => ({ count: prev.count + 1 }))
+  const handleChange = (event) => {
+    setInputNama(event.target.value)
   }
 
-  testMethod() {
-    if (this.setState.count === 5) {
-      this.setState({
-        text: "Selamat anda telah membuat lifecycle class component"
-      })
-    }
+  const handleEdit = (event) => {
+    let index = parseInt(event.target.value)
+    let editValue = namaPeserta[index]
+    setInputNama(editValue)
+    setCurrentIndex(event.target.value)
   }
 
-  render() {
-    return (
-      <>
-        <div>
-          <p>Anda telah mengklik {this.setState.count} kali</p>
-        </div>
-        <button onClick={this.handleButton}>Klik</button>
-      </>
-    )
+  const handleDelete = (event) => {
+    let index = parseInt(event.target.value)
+    let deletedItem = namaPeserta[index]
+    let newData = namaPeserta.filter((e) => {
+      return e !== deletedItem
+    })
+    console.log(newData)
+    setNamaPeserta(newData)
   }
+
+  return (
+    <>
+      <h1>List Nama Peserta Pegajuan Pinjaman CRUD</h1>
+      <table className="namaTabel">
+        <thead>
+          <tr>
+            <th>Nomor</th>
+            <th>Nama Pengaju</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            namaPeserta.map((value, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{value}</td>
+                  <td>
+                    <button onClick={handleEdit} value={index}>Edit</button>
+                    <button onClick={handleDelete} value={index}>Delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+
+      {/* Form */}
+      <form onSubmit={handleSubmit}>
+        <label>Masukan Nilai : </label>
+        <input type="text" value={inputNama} onChange={handleChange}></input>
+        <button>Simpan Data</button>
+      </form>
+    </>
+  )
 }
 
 export default Latihan
--- src/WelcomeComponent.js
+++ src/WelcomeComponent.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Welcome(props) {
-    return <h1>Hallo, {props.name}</h1>
+const Latihan = (props) => {
+    return <h1>Selamat Pagi {props.nama}</h1>
 }
 
-export default Welcome
+export default Latihan
